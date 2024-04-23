c7f2811f349da7f6c1daddb97acda9e2f5165dbe
MonkeyShock3000 <167050968+MonkeyShock3000@users.noreply.github.com>
2024-04-23 19:56:23 -0400

    Add banana

---
+++ Banana.java
@@ -0,0 +1,10 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+
+public class Banana extends Actor
+{
+        public void act()
+    {
+        move(-3);
+    }
+}
