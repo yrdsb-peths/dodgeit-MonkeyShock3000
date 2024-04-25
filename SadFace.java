daf920839178e2a272837e136bb4b215f8edd78c
MonkeyShock3000 <167050968+MonkeyShock3000@users.noreply.github.com>
2024-04-25 10:06:00 -0400

    Changed sad face

--- SadFace.java
+++ SadFace.java
@@ -1,11 +1,6 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-/**
- * Write a description of class SadFace here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
+
 public class SadFace extends Actor
 {
     /**
