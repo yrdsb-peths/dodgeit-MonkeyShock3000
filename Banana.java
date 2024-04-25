8ffb2a51e2ab2479c7d5e460f343302a2b051359
MonkeyShock3000 <167050968+MonkeyShock3000@users.noreply.github.com>
2024-04-25 09:58:10 -0400

    Add sad face

--- Banana.java
+++ Banana.java
@@ -11,6 +11,14 @@ public class Banana extends Actor
         {
             resetBanana();
         }
+        
+        if (isTouching(Hero.class))
+        {
+            // Add a gameover symbol
+            SadFace sadFace = new SadFace();
+            getWorld().addObject(sadFace, 300, 200);
+            getWorld().removeObject(this);
+        }
     }
     
     public void resetBanana()
